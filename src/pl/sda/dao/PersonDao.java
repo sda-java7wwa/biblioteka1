980807d258270a57a05225450dce78ac0e6c7ee9
Krzysztof Krupa <krzysztof.s.krupa@gmail.com>
2018-03-02 19:51:25 +0100

    metody w interfejscie PresonDao

--- src/pl/sda/dao/PersonDao.java
+++ src/pl/sda/dao/PersonDao.java
@@ -1,7 +1,19 @@
 package pl.sda.dao;
 
+import pl.sda.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by x on 2018-03-02 at 19:11
  */
 public interface PersonDao {
+
+    //zwrocimy obiety typu person a jeszcze lepiej optional (opakowac go dodoatkowo)
+    Optional<Person> getPersonByLogin (String login);
+
+
+    //przekazujemy liste klientow ktorych chcemy zapisac
+    boolean savePersons(List<Person> persons);
 }
