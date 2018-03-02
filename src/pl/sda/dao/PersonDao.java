a72c2e31eb629672fe9cfabf2d0ea8116f0bc5d9
Krzysztof Krupa <krzysztof.s.krupa@gmail.com>
2018-03-02 19:59:37 +0100

    metody w interfejscie PresonDao poprawione

--- src/pl/sda/dao/PersonDao.java
+++ src/pl/sda/dao/PersonDao.java
@@ -10,10 +10,16 @@ import java.util.Optional;
  */
 public interface PersonDao {
 
-    //zwrocimy obiety typu person a jeszcze lepiej optional (opakowac go dodoatkowo)
-    Optional<Person> getPersonByLogin (String login);
+    //dwie metody
+    //metoda , logika biznesowa nie bedzie wiedziala skad pobiera , ona dostaje klienta i juz
+    //zapisuje klientow z kontami
 
+    List<Person> getPersons();
+
+    //metoda do zapisu , metoda zwraca boolean
+    // nie udalo sie zapisac bo costam
 
     //przekazujemy liste klientow ktorych chcemy zapisac
-    boolean savePersons(List<Person> persons);
+    boolean savePersons(List<Person> clients);
+
 }
