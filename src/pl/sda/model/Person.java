bda56d9b40ebd57166b17c55603c0b44d1028bf4
Michal Sadowski <michal.sadovski@gmail.com>
2018-03-02 19:26:12 +0100

    stworzenie klasy Person

---
+++ src/pl/sda/model/Person.java
@@ -0,0 +1,61 @@
+package pl.sda.model;
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+
+    public Person(String name, String surname, String login, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(surname, person.surname) &&
+                Objects.equals(login, person.login) &&
+                Objects.equals(password, person.password);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, surname, login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
