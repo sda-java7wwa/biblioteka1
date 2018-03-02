9389af6391430361d40e549acbbebc4b695b40d4
Julia Ivaniychuk <yulia_ivaniychuk@onet.pl>
2018-03-02 21:11:56 +0100

    add service three commit

--- src/pl/sda/service/PersonService.java
+++ src/pl/sda/service/PersonService.java
@@ -6,6 +6,6 @@ import pl.sda.model.Person;
  * Created by yulia on 02.03.2018 at 19:16
  */
 public interface PersonService {
-    boolean singUp(String name, String surname, String login, String passowd);
+    boolean signUp(String name, String surname, String login, String password1, String password2);
     boolean login(String login, String password);
 }
--- src/pl/sda/service/PersonServiceImplementation.java
+++ src/pl/sda/service/PersonServiceImplementation.java
@@ -1,32 +1,37 @@
 package pl.sda.service;
 
 import pl.sda.dao.PersonDao;
+import pl.sda.dao.PersonDaoFile;
 import pl.sda.model.Person;
 
 /**
  * Created by yulia on 02.03.2018 at 19:17
  */
 public class PersonServiceImplementation implements PersonService{
-    private PersonDao personDao;
+    private PersonDao personDao = new PersonDaoFile();
 
     @Override
-    public boolean singUp(String name, String surname, String login, String password) {
-//        if (!login.equals()){
-//            new Person(name, surname, login, password);
-//            return true;
-//        } else {
-//            return false;
-//        }
+    public boolean signUp(String name, String surname, String login, String password1, String password2) {
+        if (loginNotExists(login)){
+            if (password1.equals(password2)) {
+                new Person(name, surname, login, password1);
+                return true;
+            }
+        }
         return false;
+    }
+
+    private boolean loginNotExists(String login){
+        return personDao.getPersons()
+                .stream()
+                .anyMatch(person -> !person.getLogin().equals(login));
     }
 
     @Override
     public boolean login(String login, String password) {
-//        if (login.equals() ){
-//            return true;
-//        } else {
-//            return false;
-//        }
-        return false;
+        return personDao.getPersons()
+                .stream()
+                .anyMatch(person -> person.getLogin().equals(login) && person.getPassword().equals(password));
     }
+
 }
