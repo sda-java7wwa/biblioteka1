4ec6fc4ed86a9c4a1f3bae1c7531c2a0c307f080
Krzysztof Krupa <krzysztof.s.krupa@gmail.com>
2018-03-02 20:38:21 +0100

    PersonDaoFile metoda1

--- src/pl/sda/dao/PersonDaoFile.java
+++ src/pl/sda/dao/PersonDaoFile.java
@@ -2,6 +2,11 @@ package pl.sda.dao;
 
 import pl.sda.model.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,13 +14,56 @@ import java.util.List;
  */
 
 //zapis i odczyt do pliku robimy poprzez Serializację i Desarializację!
-public class PersonDaoFile implements PersonDao{
+public class PersonDaoFile implements PersonDao {
+
+    private static final String FILE_NAME = "datasource.txt";
+
+    private static final String PERSON = "PERSON";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String DELIMITER = ";";
+    // private static final String PASSOWRD = "PASSWORD";
+
+    private static final Path PATH = Paths.get(FILE_NAME);
 
 
     @Override
     public List<Person> getPersons() {
-        return null;
+
+        List<Person> persons = new ArrayList<>();
+
+        try {
+            List<String> lines = Files.readAllLines(PATH);
+
+            for (String line : lines) {
+                String[] tab = line.split(DELIMITER);
+
+
+                if (tab[0].equals(PERSON)) {
+
+                    persons.add(getPersonWithGivenParams(tab));
+//                }else if(tab[0].equals(PASSWORD)){
+//
+//                    persons.get(persons.size()-1)
+//                            .getPassword()
+//                            .
+//                }
+
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return persons;
     }
+
+
+
+    private Person getPersonWithGivenParams(String[] params) {
+        return new Person(params[1], params[2], params[3], params[4], params[5], null);
+    }
+
 
     @Override
     public boolean savePersons(List<Person> clients) {
