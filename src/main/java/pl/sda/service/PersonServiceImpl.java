ba1bda7fe50a039121b2a460373f629e27b44158
Julia Ivaniychuk <yulia_ivaniychuk@onet.pl>
2018-03-06 17:59:48 +0100

    zmiana login i sign up

--- src/main/java/pl/sda/service/PersonServiceImpl.java
+++ src/main/java/pl/sda/service/PersonServiceImpl.java
@@ -1,8 +1,11 @@
 package pl.sda.service;
 
+import pl.sda.Exception.InvalidUserException;
 import pl.sda.dao.PersonDao;
 import pl.sda.dao.PersonDaoFile;
 import pl.sda.model.Person;
+
+import java.util.stream.Stream;
 
 /**
  * Created by yulia on 02.03.2018 at 19:17
@@ -11,19 +14,26 @@ public class PersonServiceImpl implements PersonService{
     private PersonDao personDao = new PersonDaoFile();
 
     @Override
-    public boolean signUp(String name, String surname, String login, String password1, String password2) {
+    public Person signUp(String name, String surname, String login, String password1, String password2) throws
+            InvalidUserException{
         try {
             if (loginNotExists(login)){
                 if (password1.equals(password2)) {
-                    personDao.savePerson(new Person(name, surname, login, password1));
-                    return true;
+                    Person person = new Person(name, surname, login, password1);
+                    personDao.savePerson(person);
+                    return person;
+                    //return personDao.getPersons().stream().findAny().get();
+                } else {
+                    throw new InvalidUserException("Passwords not equal");
                 }
+            } else {
+                throw new InvalidUserException("This login is already taken");
             }
+
         } catch (NullPointerException e){
             System.out.println("Stream is empty");
-            return false;
         }
-        return false;
+        return null;
     }
 
     private boolean loginNotExists(String login){
@@ -33,10 +43,17 @@ public class PersonServiceImpl implements PersonService{
     }
 
     @Override
-    public boolean login(String login, String password) {
-        return personDao.getPersons()
-                .stream()
-                .anyMatch(person -> person.getLogin().equals(login) && person.getPassword().equals(password));
+    public Person login(String login, String password) throws InvalidUserException{
+        if (personDao.getPersons().stream().anyMatch(person -> person.getLogin().equals(login)
+                && person.getPassword().equals(password))){
+            return personDao.getPersons()
+                    .stream()
+                    .filter(person -> person.getLogin().equals(login) && person.getPassword().equals(password))
+                    .findFirst()
+                    .get();
+        } else {
+            throw new InvalidUserException("Invalid login or password");
+        }
     }
 
 }
