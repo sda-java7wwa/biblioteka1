7db8e9b9f3378243422c0a39ecdb6d61604a7102
Julia Ivaniychuk <yulia_ivaniychuk@onet.pl>
2018-03-06 18:00:38 +0100

    interface

--- src/main/java/pl/sda/service/PersonService.java
+++ src/main/java/pl/sda/service/PersonService.java
@@ -1,11 +1,12 @@
 package pl.sda.service;
 
+import pl.sda.Exception.InvalidUserException;
 import pl.sda.model.Person;
 
 /**
  * Created by yulia on 02.03.2018 at 19:16
  */
 public interface PersonService {
-    boolean signUp(String name, String surname, String login, String password1, String password2);
-    boolean login(String login, String password);
+    Person signUp (String name, String surname, String login, String password1, String password2) throws InvalidUserException;
+    Person login (String login, String password) throws InvalidUserException;
 }
