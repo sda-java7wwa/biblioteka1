230cab096a540b94752000d537473875b37334d5
Julia Ivaniychuk <yulia_ivaniychuk@onet.pl>
2018-03-06 18:06:34 +0100

    add exception

---
+++ src/main/java/pl/sda/Exception/InvalidUserException.java
@@ -0,0 +1,10 @@
+package pl.sda.Exception;
+
+/**
+ * Created by yulia on 05.03.2018 at 22:37
+ */
+public class InvalidUserException extends Exception {
+    public InvalidUserException(String message){
+        super(message);
+    }
+}
