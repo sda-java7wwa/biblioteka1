c104a16e70f1fa1ffe6e891436fab0eb4a4fbdc4
Julia Ivaniychuk <yulia_ivaniychuk@onet.pl>
2018-03-08 18:19:53 +0100

    add interface BookCategoriService

---
+++ src/main/java/pl/sda/service/BookCategoryService.java
@@ -0,0 +1,10 @@
+package pl.sda.service;
+
+import pl.sda.model.BookCategory;
+
+/**
+ * Created by yulia on 07.03.2018 at 21:35
+ */
+public interface BookCategoryService {
+    void showCategory(BookCategory bookCategory);
+}
