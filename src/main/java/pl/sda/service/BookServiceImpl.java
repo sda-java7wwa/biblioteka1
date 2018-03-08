b9208eb085d7980f6a8f58d143b26f8716ee88c4
Julia Ivaniychuk <yulia_ivaniychuk@onet.pl>
2018-03-08 18:41:23 +0100

    add name file

--- src/main/java/pl/sda/service/BookServiceImpl.java
+++ src/main/java/pl/sda/service/BookServiceImpl.java
@@ -1,19 +1,23 @@
 package pl.sda.service;
 
 import pl.sda.bookEnum.BookStatus;
+import pl.sda.dao.LibraryDaoFile;
 import pl.sda.model.Book;
 
 import java.util.Iterator;
 import java.util.List;
 
 public class BookServiceImpl implements BookService {
-    private List<Book> bookList;
+    private LibraryDaoFile libraryDaoFile;
 
+    public BookServiceImpl(String fileName) {
+        this.libraryDaoFile = new LibraryDaoFile(fileName);
+    }
 
 
     @Override
     public void showBooks() {
-       Iterator<Book> it = bookList.iterator();
+       Iterator<Book> it = libraryDaoFile.getListOfBooks().iterator();
        while (it.hasNext()) {
           System.out.println(it.next());
        }
