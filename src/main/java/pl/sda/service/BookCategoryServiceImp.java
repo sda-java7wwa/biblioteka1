6b6b86eb2bf069de9b710db83f7483d6e633f950
Julia Ivaniychuk <yulia_ivaniychuk@onet.pl>
2018-03-08 18:20:19 +0100

    add class BookCategoriServiceImp

---
+++ src/main/java/pl/sda/service/BookCategoryServiceImp.java
@@ -0,0 +1,15 @@
+package pl.sda.service;
+
+import pl.sda.model.BookCategory;
+
+/**
+ * Created by yulia on 07.03.2018 at 21:36
+ */
+public class BookCategoryServiceImp implements BookCategoryService {
+    private BookCategory bookCategory;
+
+    @Override
+    public void showCategory(BookCategory bookCategory) {
+
+    }
+}
